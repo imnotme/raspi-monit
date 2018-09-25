e973ba183de7971e83bdd43617de142347865f2e
imnotme <ten2xv@protonmail.com>
2018-09-26 00:28:16 +0200

    Public IP monitoring, alt version
    In order to get the public IP sent by mail, I had to use a script instead of file monitoring

---
+++ script/ipinfo.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+# exit
+# 0 : Ok IP not changed
+# 1 : New IP
+# 2 : Error while getting new IP
+# 3 : Optionnal, exit if new file have been created
+
+IPFILE=/tmp/ipinfo.result
+#HASHBIN=/usr/bin/md5sum
+
+if [ ! -e $IPFILE ]
+	then
+		/usr/bin/curl http://ipinfo.io -o $IPFILE > /dev/null 2>&1
+		echo New IP temp file created
+		#exit 3
+fi
+
+
+# Variable order IS important
+OLDIP=$(/bin/cat $IPFILE)
+NEWIP=$(/usr/bin/curl http://ipinfo.io/ip -o $IPFILE > /dev/null 2>&1; /bin/cat $IPFILE)
+#OLDIPHASH=$($HASHBIN $OLDIP)
+#NEWIPHASH=$($HASHBIN $OLDIP)
+
+#Debug
+#echo OLD : $OLDIP
+#echo NEW : $NEWIP
+
+if [ "$NEWIP" != "$OLDIP" ]
+	then
+		if [ -z "$NEWIP" ]
+			then
+				echo Error while getting new IP address
+				exit 2
+			else
+				echo New IP address \: $NEWIP
+				exit 1
+		fi
+	else
+		echo $OLDIP
+		exit 0
+fi
+# We should never get there
+exit 128
